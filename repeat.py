f6db94d3714e09c2062c5f8ccf402a0b2c662a8c
vitaedrinker <vitaedrinker@gmail.com>
2017-12-14 11:13:24 -0600

    Add files via upload

--- repeat.py
+++ repeat.py
@@ -5,13 +5,18 @@ import unicodedata
 
 print("All files must be .txt files.")
 name = input("What is the file name? ")+".txt"
-outputname = input("What would you like to call the output?")+".txt"
-now = datetime.datetime.now().strftime("%Y-%m-%d "".txt")
+outputname = input("What would you like to call the output? ")+".txt"
+now = datetime.datetime.now().strftime("%Y-%m-%d ")+outputname
 type(name)
 
 lines_seen = set() 														# holds lines already seen
 outfile = open(now, "w+")
-for line in open (name, "r", encoding='utf-8'):
+
+#f=open(name, "a")
+#for i in range (2):
+#	f.write("")
+
+for line in open (name, "r", encoding='ascii'):
 	if line not in lines_seen:											# not a duplicate
 		outfile.write(line)
 		lines_seen.add(line)
