0abd846706f5ef386f380a731d462a9dcfaef3d3
vitaedrinker <vitaedrinker@gmail.com>
2017-12-14 10:05:43 -0600

    Add files via upload

--- repeat.py
+++ repeat.py
@@ -1,14 +1,17 @@
 # Compare two lines of a text file and look for repeats
 
 import datetime
+import unicodedata
 
-now = datetime.datetime.now().strftime("%Y-%m-%d Phishing URLs.txt")
+print("All files must be .txt files.")
 name = input("What is the file name? ")+".txt"
+outputname = input("What would you like to call the output?")+".txt"
+now = datetime.datetime.now().strftime("%Y-%m-%d "".txt")
 type(name)
 
 lines_seen = set() 														# holds lines already seen
 outfile = open(now, "w+")
-for line in open (name, "r"):
+for line in open (name, "r", encoding='utf-8'):
 	if line not in lines_seen:											# not a duplicate
 		outfile.write(line)
 		lines_seen.add(line)
